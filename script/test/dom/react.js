98b43c7d498d6443d9d969973d4f36ee06d84a1a
ldj <ldj@gfloan.com>
2020-08-04 23:03:58 +0800

    react

---
+++ script/test/dom/react.js
@@ -0,0 +1,25 @@
+var model = {
+	a: 10,
+	b: 1
+}
+
+var map = {
+	dom: function(obj) {
+		return `a=[${obj.a}],b=[${obj.b}]`
+	}
+}
+
+
+function bindModelMap(obj, map) {
+
+}
+
+
+
+var data = bindModelMap(model, map);
+
+
+model.a=100;
+
+
+console.log(data.dom);
